a26d3ed91a556712c2517314f52d48280d434d2b
Purrbiscuit <spurr@articulate.com>
2021-01-30 16:12:31 -0500

    finish simple-server exercise

--- simple_server/main.js
+++ simple_server/main.js
@@ -1,1 +1,21 @@
-console.log("hello")
+'use strict'
+
+const http = require('http')
+const httpStatus = require('http-status-codes')
+const port = 3000
+
+const serverHandler = (req, res) => {
+  console.log(`Request received at URL ${req.url}`)
+
+  res.writeHead(httpStatus.OK, {
+    'Content-Type': 'text/html'
+  })
+  
+  res.write('<h1>Hello, Universe!</h1>')
+  res.end()
+}
+
+const server = http.createServer(serverHandler)
+
+server.listen(port)
+console.log(`The server has started and is listening on port number: ${port}`)
